9c404177285d71661d2f8550be618e7493f25718
fahziar <fahziar_rw@yahoo.co.uk>
2015-04-28 19:23:53 +0700

    Initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="navbar navbar-default navbar-fixed-bottom">
+  <div class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span12"> &copy; 2015 Dinas Perpajakan Kota Bandung. </div>
+        <!-- /span12 --> 
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /footer-inner --> 
+</div>
+<!-- /footer --> 
